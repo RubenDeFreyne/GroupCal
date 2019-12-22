e92eab852657334a9d8d7b465f5f00e0283d5216
Ruben De Freyne <ruben.defreyne.y0592@student.hogent.be>
2019-12-22 15:14:12 +0100

    GroupViewModel Implementation

--- GroupCal/app/src/main/java/com/example/groupcal/viewmodels/GroupViewModel.kt
+++ GroupCal/app/src/main/java/com/example/groupcal/viewmodels/GroupViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.groupcal.viewmodels
 
+import androidx.lifecycle.ViewModel
+import com.example.groupcal.data.GroupRepository
+import com.example.groupcal.models.Group
+
+class GroupViewModel : ViewModel() {
+    /* After Merging Room
+    private val _categories: MutableLiveData<List<Category>> = MutableLiveData()
+    val categories: LiveData<List<Category>> = _categories
+     */
+
+    private val groupRepository = GroupRepository()
+    var groups: List<Group> = ArrayList<Group>()
+
+    fun getGroups(){
+        groupRepository.initializeGroups()
+        groups = groupRepository.groups
+    }
+
+}
