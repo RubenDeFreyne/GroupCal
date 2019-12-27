2a6a2f5c20821be68c2a741d50981cafd36c306b
Ruben De Freyne <ruben.defreyne.y0592@student.hogent.be>
2019-12-27 22:17:58 +0100

    Api refactor

--- GroupCal/app/src/main/java/com/example/groupcal/data/network/EventApi.kt
+++ GroupCal/app/src/main/java/com/example/groupcal/data/network/EventApi.kt
@@ -1,16 +1,33 @@
 package com.example.groupcal.data.network
 
 import com.example.groupcal.models.Event
-import com.example.groupcal.models.Group
 import kotlinx.coroutines.Deferred
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
+/**
+ * Interface for sending requests related to [Event] to backend. This done by Retrofit.
+ *
+ * This is injected by koin in all the classes that use it
+ */
 interface EventApi {
 
+    /**
+     * Get a list of the events saved in the backend
+     *
+     * @return Deferred list of events in the backend
+     */
     @GET("events")
     fun getEvents() : Deferred<List<Event>>
+
+    /**
+     * Create a new event in the backend
+     *
+     * @param event The event that has to be created in the backend
+     *
+     * @return Deferred of the created event
+     */
 
     @POST("events")
     fun addEvent(@Body event: Event): Deferred<Event>
